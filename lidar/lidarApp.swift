ca124a8b711c3786b32a8ef9501fdbd404bb0a88
Anastasia Rudenko <nastyarudenko@icloud.com>
2021-03-29 16:26:07 +0300

    Initial Commit

---
+++ lidar/lidarApp.swift
@@ -0,0 +1,17 @@
+//
+//  lidarApp.swift
+//  lidar
+//
+//  Created by Anastasia Rudenko on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct lidarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
